c1d4c00eaa00d67cb21cabff12772b36c3b41d60
amandadrehmer <amandasilveira.rs@gmail.com>
2023-09-05 23:37:32 -0300

    Consumindo API

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React from 'react';
-import Header from './components/Header/Header';
 import StyledGlobal from './styledGlobal';
-import Perfil from './pages/Perfil/VisualizarPerfil'
-// import Login from './pages/Login/Login';
-// import Sidebar from './components/Nav/Nav';
+import Login from './pages/Login/Login'
 
 const App = () => {
   return (
     <div>
-      <StyledGlobal/>
-      <Header />
-      {/* <Sidebar/> */}
-      {/* <Login /> */}
-      <Perfil/>
+      <StyledGlobal />
+      <Login />
     </div>
   );
 };
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,29 +1,62 @@
-import React from 'react';
+import { useState } from 'react';
+import Header from '../../components/Header/Header'
 import { CustomForm, CustomInput, CustomInputSubmit, CustomLabel, CustomLink, FormContainer, TitleDoIt } from './styledLogin';
+import axios from 'axios';
 
 function Login() {
+
   document.body.style.overflow = 'hidden'; //  Corta o conteúdo que ultrapassa o tamanho da DIV
   
+  const [email, setEmail] = useState = ('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    const credenciais = { email, password }
+    
+    axios.post('https://localhost:8000/login', credenciais, {
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    }).then(response => {
+      alert(response.data.message)
+    })
+      .catch(error => console.log(error))
+    
+  };
+  
   return (
-    <FormContainer>
-      
-      <TitleDoIt>Welcome back to Do It!</TitleDoIt>
-
-      <CustomForm>
-
-        <CustomLabel>Email</CustomLabel>
-        <CustomInput type="text" id="usuario" placeholder="0632454567@senacrs.edu.br" />
+    <>
+      <Header />
+      <FormContainer>
         
-        <CustomLabel>Password</CustomLabel>
-        <CustomInput type="password" id="senha" placeholder="Enter your password" />
+        <TitleDoIt>Welcome back to Do It!</TitleDoIt>
 
-        <CustomInputSubmit class="submit" id="submit" value="Enter" type="submit" />
-        
-      </CustomForm>
+        <CustomForm onSubmit={handleSubmit}>
 
-      <p>Not registred yet? <CustomLink href="">Create an account</CustomLink></p>
+          <CustomLabel>Email</CustomLabel>
+          <CustomInput type="text"
+            id="usuario"
+            placeholder="0632454567@senacrs.edu.br"
+            value={email}
+            onChange = {(e) => setEmail(e.target.value)}
+          />
+          
+          <CustomLabel>Password</CustomLabel>
+          <CustomInput type="password"
+            id="senha"
+            placeholder="Enter your password"
+            value={password} 
+            onChange = {(e) => setPassword(e.target.value)}  
+          />
 
-    </FormContainer>
+          <CustomInputSubmit class="submit" id="submit" value="Enter" type="submit" />
+          
+        </CustomForm>
+
+        <p>Not registred yet? <CustomLink href="">Create an account</CustomLink></p>
+
+      </FormContainer>
+      </>
   );
 };
 
